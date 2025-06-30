fa3313e24d9e8e2fb68446cdff443bf96162335c
herdssman <matthew.hf@outlook.com>
2025-06-30 21:28:09 +0200

    move file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import css from './App.module.css'
-import  CafeInfo  from '../cafe-info/cafe-info'
+import  CafeInfo  from '../CafeInfo/cafe-info'
 import type { VoteType } from '../../types/votes'
-import VoteOptions from '../vote-options/vote-options'
-import VoteStats from '../vote-stats/vote-stats'
+import VoteOptions from '../VoteOptions/vote-options'
+import VoteStats from '../VoteStats/vote-stats'
 import Notifications from '../notifications/notifications'
 
 
